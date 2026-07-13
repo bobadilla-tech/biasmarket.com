399cb32ffe7518e5ec392807f58f15b510a5fbf0
OneLastChanc3 <ytcarlos8045@gmail.com>
2026-07-13 14:39:00 -0500

    add stores and product  modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StoresModule } from './modules/stores/stores.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [],
+  imports: [StoresModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
