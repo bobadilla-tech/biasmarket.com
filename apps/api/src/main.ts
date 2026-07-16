bffaba20ea4857ea65fcbce3d7e3af6afe686b0b
Eliaz Bobadilla <eliaz@bobadilla.tech>
2026-07-16 14:37:56 -0500

    tweaks: initial run

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -6,15 +8,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: false,
   });
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+  
   app.enableCors({
     origin: process.env.WEB_URL ?? 'http://localhost:3001',
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
